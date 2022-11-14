a9c14735f560b0965e485cf3136e136e6cb1e52c
Bibhor2000 <daxter740@gmail.com>
2022-11-14 13:53:07 -0500

    finished making data list for types of eyes

---
+++ src/data/eyelist.js
@@ -0,0 +1,17 @@
+const eyeList = [
+    'variant01',
+    'variant02',
+    'variant03',
+    'variant04',
+    'variant05',
+    'variant06',
+    'variant07',
+    'variant08',
+    'variant09',
+    'variant10',
+    'variant11',
+    'variant12',
+    'variant13'
+]
+
+export default eyeList;
