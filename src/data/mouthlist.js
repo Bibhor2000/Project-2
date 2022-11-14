e46432354963ed783be66f4f2fc86a81cf0bcc1f
Bibhor2000 <daxter740@gmail.com>
2022-11-14 13:56:39 -0500

    finished making data list for types of mouth

---
+++ src/data/mouthlist.js
@@ -0,0 +1,24 @@
+const mouthList = [
+    'happy01',
+    'happy02',
+    'happy03',
+    'happy04',
+    'happy05',
+    'happy06',
+    'happy07',
+    'happy08',
+    'happy09',
+    'sad01',
+    'sad02',
+    'sad03',
+    'sad04',
+    'sad05',
+    'sad06',
+    'sad07',
+    'sad08',
+    'surprised01',
+    'surprised02',
+    'surprised03'
+]
+
+export default mouthList;
