cb0bca7f14596e83b7fc7963d89104dbb787366c
Bibhor2000 <daxter740@gmail.com>
2022-11-14 15:40:59 -0500

    stuck on the dropdowns

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,8 +3,54 @@ import {useParams, Link} from 'react-router-dom';
 
 function Dropdown ({character, setCharacter, info, setInfo, input, setInput, clothingList, eyeBrowList, eyeList, hairColorList, hairList, mouthList, skinColorList, getInfo, getInput, getCharacter}) {
 
+    let clothes = clothingList.map(c => {
+        return (
+            <option>{c}</option>
+        ) 
+    })
+
+    let eyebrows = eyeBrowList.map(eyb => {
+        return (
+            <option>{eyb}</option>
+        )
+    })
+
+    let eyes = eyeList.map(ey => {
+        return (
+            <option>{ey}</option>
+        )
+    })
+
+    let hairs = hairList.map(h => {
+        return (
+            <option>{h}</option>
+        )
+    })
+
+    let hairColors = hairColorList.map(hc => {
+        return (
+            <option>{hc}</option>
+        )
+    })
+
+    let skinColors = skinColorList.map(s => {
+        return (
+            <option>{s}</option>
+        )
+    })
+
+    let mouths = mouthList.map(m => {
+        return (
+            <option>{m}</option>
+        )
+    })
+
     return (
         <div>
+            <div>
+            
+            </div>
+            
         </div>
     )
 }
