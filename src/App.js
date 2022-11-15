1202b8660ce0a12298a940795ce80a6abc70ed40
Bibhor2000 <daxter740@gmail.com>
2022-11-15 14:29:11 -0500

    removed some more unused props

--- src/App.js
+++ src/App.js
@@ -18,43 +18,19 @@ import skinColorList from './data/skincolorlist';
 function App() {
   const [character, setCharacter] = useState()
   const [info, setInfo] = useState()
-  const [input, setInput] = useState({
-    // clothes: '',
-    // eyebrow: '',
-    // eye: '',
-    // hair: '',
-    // hairColor: '',
-    // mouth: '',
-    // skinColor: ''
-  })
+  const [input, setInput] = useState()
   const [image, setImage] = useState('')
-  //console.log(info)
-  // const url = 'https://avatars.dicebear.com/api/pixel-art/avatar.svg?eyes[]'
-  // const readUrl = url.data
-
-  // async function getInfo () {
-  //   const readInfo = await axios.get(`https://avatars.dicebear.com/api/pixel-art/avatar.svg?eyes[]=variant01&eyebrows[]=variant01&mouth[]=happy01&hair[]=short01&hairColor[]=variant01&skinColor[]=variant01&clothing[]=variant01`)
-  //   setInfo(readInfo.data)
-  // }
-  //getInfo()
-
-  // async function getInput () {
-
-  // }
 
   async function getCharacter () {
     const createCharacter = await axios.get(`https://avatars.dicebear.com/api/pixel-art/avatar.svg?eyes[]=${input.eyes}&eyebrows[]=${input.eyebrows}&mouth[]=${input.mouths}&hair[]=${input.hairs}&hairColor[]=${input.hairColors}&skinColor[]=${input.skinColors}&clothing[]=${input.clothes}`)
     setImage(createCharacter)
   }
-  // console.log(image)
-  // console.log(`https://avatars.dicebear.com/api/pixel-art/avatar.svg?eyes[]=${input.eyes}&eyebrows[]=${input.eyebrows}&mouth[]=${input.mouths}&hair[]=${input.hairs}&hairColor[]=${input.hairColors}&skinColor[]=${input.skinColors}&clothing[]=${input.clothes}`)
 
   return (
     <div>
       <Header/>
       <Container 
       character={character} setCharacter={setCharacter}
-      info={info} setInfo={setInfo}
       input={input} setInput={setInput}
       clothingList={clothingList}
       eyeBrowList={eyeBrowList}
--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,14 +1,15 @@
 import React, {Component, useState, useEffect} from 'react';
 import {useParams, Link} from 'react-router-dom';
+import SVG from 'react-inlinesvg';
 
 function Icon ({character, setCharacter, clothingList, eyeBrowList, eyeList, hairColorList, hairList, mouthList, skinColorList, getCharacter, image}) {
 
+    const mySVG = image.data;
     return (
         <div>
             <button onClick={(e) => {e.preventDefault()
             getCharacter()}}>Click me</button>
-            {/* <img src={image.data}/> */}
-            {image.data}
+            <SVG src={mySVG} />
         </div>
     )
 }
