f113f9cfb9f9d48d9cfe317e8076ee3cb542a636
Bibhor2000 <daxter740@gmail.com>
2022-11-16 18:36:25 -0500

    added buttons to play and pause background music

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,21 @@
 import React, {Component, useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import audio from './audios/audio.mp3';
 
 function Footer () {
+    const bgm = new Audio(audio)
+    const start = () => {
+        bgm.play()
+    }
+    const stop = () => {
+        bgm.pause()
+    }
 
     return (
-        <div>This is Footer</div>
+        <div>
+            <button onClick={start}>Play</button>
+            <button onClick={stop}>Pause</button>
+        </div>
     )
 }
 
--- src/components/QuickDocs.js
+++ src/components/QuickDocs.js
@@ -6,12 +6,13 @@ function QuickDocs ({character, setCharacter, info, setInfo, input, setInput, cl
     return (
         <div>
             <div className='Tips'> 
-                <h2>Before you begin:</h2>
+                <h3>Before you begin:</h3>
                 <p>Make sure you select every option before creating a character</p>
                 <p>If you want to use the default values, just select another option and re-select it</p>
             </div>
             <div className='Site'>
-                <Link href='https://avatars.dicebear.com/styles/pixel-art'>DiceBear Avatars</Link>
+                <h3>If you want to know more about the used API click on the link below:</h3>
+                <a href='https://avatars.dicebear.com/styles/pixel-art'>DiceBear Avatars</a>
             </div>
             <div className='Reference'>
             </div>
