314c5dc3827b09eb9e8998594532dd5f10cdad20
Bibhor2000 <daxter740@gmail.com>
2022-11-17 09:25:18 -0500

    reference list for skin colors finished

--- src/components/QuickDocs.js
+++ src/components/QuickDocs.js
@@ -21,28 +21,50 @@ function QuickDocs ({character, setCharacter, info, setInfo, input, setInput, cl
                     <li>Variant01 = Looking Away</li>
                     <li>Variant02 = Filled with Guilt</li>
                     <li>Variant03 = Looking Up</li>
+                    <li>Variant04 = Surprised</li>
+                    <li>Variant05 = Looking Down</li>
+                    <li>Variant06 = Narrowed</li>
+                    <li>Variant07 = Two-Tone</li>
+                    <li>Variant08 = Cross-Eyed</li>
+                    <li></li>
                 </ul>
                 <ul>
                     <h4>Eyebrow Types</h4>
                     <li>Variant01 = Normal</li>
                     <li>Variant02 = None</li>
                     <li>Variant03 = Angry</li>
+                    <li>Variant04 = Raised</li>
+                    <li>Variant05 = Normal02</li>
+                    <li>Variant06 = Arched</li>
+                    <li>Variant07 = Arched02</li>
+                    <li>Variant08 = Normal03</li>
                 </ul>
                 <ul>
                     <h4>Hair Colors</h4>
                     <li>Variant01 = Blonde</li>
-                    <li>Variant02 = Brown</li>
-                    <li>Variant03 = Brown02</li>
+                    <li>Variant02 = Blonde02</li>
+                    <li>Variant03 = Brown</li>
+                    <li>Variant04 = Brown02</li>
+                    <li>Variant05 = DarkBrown</li>
+                    <li>Variant06 = DarkBrown02</li>
+                    <li>Variant07 = Maroon</li>
+                    <li>Variant08 = Maroon02</li>
                 </ul>
                 <ul>
                     <h4>Skin Colors</h4>
                     <li>Variant01 = Starting point for lighter skin tone</li>
+                    <li>Variant04 = Starting point for medium skin tone</li>
                 </ul>
                 <ul>
                     <h4>Clothes</h4>
                     <li>Variant01 = Striped T-Shirt</li>
-                    <li>Variant02 = V-Neck Shirt</li>
+                    <li>Variant02 = V-Neck T-Shirt</li>
                     <li>Variant03 = Tank Top</li>
+                    <li>Variant04 = V-Neck 02</li>
+                    <li>Variant05 = V-Neck 03</li>
+                    <li>Variant06 = Two-Tone T-Shirt</li>
+                    <li>Variant07 = Two-Tone Long Sleeves </li>
+                    <li>Variant08 = Collared Two-Tone T-Shirt</li>
                 </ul>
             </div>
         </div>
